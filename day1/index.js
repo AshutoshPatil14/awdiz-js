83117e2b3bcfbe4b70d7e0f149041b52e0a4e3c4
Ashutosh Patil <ashutosh.patil1409@gmail.com>
2025-06-21 22:15:41 +0530

    Day 1 of learning JavaScript

--- day1/index.js
+++ day1/index.js
@@ -1,2 +1,36 @@
-let str="Hello World!!!"
-console.log(str)
+let str="Hello "
+console.log(str, typeof "Hello World!!!")
+
+var num=12345
+console.log(num, typeof num)
+
+const bool=true
+console.log(bool, typeof true)
+
+let nullValue=null
+console.log(nullValue, typeof nullValue)
+
+var undefinedValue
+console.log(undefinedValue, typeof undefined)
+
+var counter=10
+counter++
+console.log("Increment Operator", counter)
+
+counter--
+console.log("Decrement Operator", counter)
+
+var str2="World!!!"
+console.log(str + str2)
+
+var num2=10
+console.log("Addition Operator", num + num2)
+console.log("Subtraction Operator", num - num2)
+console.log("Multiplication Operator", num * num2)
+console.log("Division Operator", num / num2)
+console.log("Modulus Operator", num % num2)
+
+var a="Hi there!"
+{
+    console.log(a)
+}
