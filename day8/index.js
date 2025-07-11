42903ceab91f72488f7d852bf42a4b7428cfab20
Ashutosh Patil <ashutosh.patil1409@gmail.com>
2025-07-11 21:07:25 +0530

    Day 9 practice

--- day8/index.js
+++ day8/index.js
@@ -1,40 +1,46 @@
-// This is the exapmle of function
-let prompt = require("prompt-sync")();
+// // This is the exapmle of function
+// let prompt = require("prompt-sync")();
 
-function addition(a, b) {
-  return a + b;
-}
-function subtraction(a, b) {
-  return a - b;
-}
+// function addition(a, b) {
+//   return a + b;
+// }
+// function subtraction(a, b) {
+//   return a - b;
+// }
 
-let num1 = Number(prompt("Enter first number: "));
-let num2 = Number(prompt("Enter second number: "));
+// let num1 = Number(prompt("Enter first number: "));
+// let num2 = Number(prompt("Enter second number: "));
 
-let add = addition(num1, num2);
-let sub = subtraction(num1, num2);
+// let add = addition(num1, num2);
+// let sub = subtraction(num1, num2);
 
-console.log("Addition: ", add, "\nSubtraction: ", sub);
+// console.log("Addition: ", add, "\nSubtraction: ", sub);
 
-let parameteredAdditon = addition(5, 10);
-console.log(
-  "This is parameterized addition where 5 & 10 are the parameters: ",
-  parameteredAdditon
-);
+// let parameteredAdditon = addition(5, 10);
+// console.log(
+//   "This is parameterized addition where 5 & 10 are the parameters: ",
+//   parameteredAdditon
+// );
 
-function greet(name) {
-  console.log("Hello, " + name + "!");
-}
+// function greet(name) {
+//   console.log("Hello, " + name + "!");
+// }
 
-let userName = prompt("Enter your name: ");
+// let userName = prompt("Enter your name: ");
 
-greet(userName);
+// greet(userName);
 
-function tableOfMultiplication(num) {
-  for (let i = 1; i <= 10; i++) {
-    console.log(num + " x " + i + " = " + num * i);
-  }
-}
+// function tableOfMultiplication(num) {
+//   for (let i = 1; i <= 10; i++) {
+//     console.log(num + " x " + i + " = " + num * i);
+//   }
+// }
 
-let number = Number(prompt("Enter a number to print a table: "));
-tableOfMultiplication(number);
+// let number = Number(prompt("Enter a number to print a table: "));
+// tableOfMultiplication(number);
+
+
+
+let folders = ["day1", "day2", "day3", "day4", "day5", "day6"]
+let toFind = prompt("Enter folder to find (ex: 'day4'):")
+
