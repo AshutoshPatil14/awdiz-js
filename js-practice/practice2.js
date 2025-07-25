5498e1d89e41724a462b67965dc3848532e9d982
Ashutosh Patil <ashutosh.patil1409@gmail.com>
2025-07-25 20:15:57 +0530

    JavaScript practice

---
+++ js-practice/practice2.js
@@ -0,0 +1,61 @@
+// Find the second highest number in an array without sorting it.
+// let array = [10, 18, 20, 8, 15];
+
+// function findSecondHighest(arr) {
+//   let max = Math.max(...arr);
+//   let filtered = arr.filter((num) => num !== max);
+//   return Math.max(...filtered);
+// }
+
+// console.log(findSecondHighest(array));
+
+// Character frequency counter from a string.
+// let str = "apple";
+
+// function frequencyCounter(string) {
+//   let obj = {};
+//   for (let i = 0; i < string.length; i++) {
+//     if (!obj[[string[i]]]) {
+//       obj[[string[i]]] = 1;
+//     } else {
+//       obj[[string[i]]]++;
+//     }
+//   }
+//   return obj;
+// }
+
+// console.log(frequencyCounter(str));
+
+
+// Reverse only words in an array of strings.
+
+// let array = ["hello","world"]
+// let newarray = []
+
+// for(let i=0; i<array.length; i++){
+//     let item = array[i].split("").reverse().join("")
+//     newarray.push(item)
+// }
+
+// console.log(newarray)
+
+
+
+// Check even or odd using map.
+// let array = [1, 2, 3, 4]
+
+// let arr = array.map(ele => ele%2==0 ? "Even" : "Odd")
+
+// console.log(arr)
+
+// Flatten a nested array (1 level deep).
+// let array = [1, 2, [3, 4], 5]
+
+// console.log(array.flat(1))
+
+
+// Print all prime numbers between 10 and 50.
+
+for(i=10; i<=50; i++){
+    if()
+}
