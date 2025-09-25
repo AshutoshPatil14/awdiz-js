832ee50ec90b91a602958d7055e30e2824545941
Ashutosh Patil <ashutosh.patil1409@gmail.com>
2025-09-25 19:14:35 +0530

    Update practice6.js

--- practice/practice6.js
+++ practice/practice6.js
@@ -3,22 +3,26 @@ let arr = [
   [4, 5, 6],
   [7, 8, 9],
 ];
-
+// Rotate 90degree clockwise
 // 7, 4, 1;
 // 8, 5, 2;
 // 9, 6, 3;
 
 function rotate90(arr) {
-
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      console.log(arr, "before");
+    for (let j = 0; j < i; j++) {
       let temp = arr[i][j];
-      arr[i][j] = arr[j][i]
+      arr[i][j] = arr[j][i];
       arr[j][i] = temp;
-      console.log(arr, "after");
     }
   }
+  console.log(arr);
+  // Reverse each row
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].reverse();
+  }
+
   return arr;
 }
-console.log(rotate90(arr));
+
+console.log(rotate90(arr)); // Call the function and log the result
