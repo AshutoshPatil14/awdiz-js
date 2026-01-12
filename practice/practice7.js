b2939a67d3d113e27882b9d0956cd73a3d47600f
Ashutosh Patil <123103890+AshutoshPatil14@users.noreply.github.com>
2026-01-12 21:24:31 +0530

    DSA Practice

---
+++ practice/practice7.js
@@ -0,0 +1,51 @@
+// Find the maximum element in an array.
+
+// Example:
+// [3, 5, 1, 8, 2] → 8
+
+// const array = [3, 5, 1, 8, 2];
+// let position = null;
+// let max = null;
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i] > max) {
+//     position = i;
+//     max = array[i];
+//   }
+// }
+// console.log(position);
+// console.log(max);
+
+// Problem 2: Second Largest Element
+
+// Input:
+// [3, 5, 1, 8, 2]
+
+// Output:
+// 5
+
+// const array = [3, 5, 1, 8, 2];
+
+// const findSecondLargest = (arr) => {
+//   let largest = 0;
+//   let secondLargest = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (largest < arr[i]) {
+//       secondLargest = largest;
+//       largest = arr[i];
+//     } else if (secondLargest < arr[i] && arr[i] !== largest) {
+//       secondLargest = arr[i];
+//     }
+//   }
+//   return secondLargest;
+// };
+
+// console.log(findSecondLargest(array));
+
+
+// Problem 3: Reverse an Array (In-Place)
+
+// Input:
+// [1, 2, 3, 4, 5]
+
+// Output:
+// [5, 4, 3, 2, 1]
