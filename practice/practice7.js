46c4b8a758e17158f5f97c8d098e3e4d81cbefa1
Ashutosh Patil <123103890+AshutoshPatil14@users.noreply.github.com>
2026-01-13 22:02:50 +0530

    Update

--- practice/practice7.js
+++ practice/practice7.js
@@ -49,3 +49,21 @@
 
 // Output:
 // [5, 4, 3, 2, 1]
+
+
+const array = [1, 2, 3, 4, 5];
+
+const reverseArray = (arr) => {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start < end) {
+    let temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+  }
+  return arr;
+};
+
+console.log(reverseArray(array));
