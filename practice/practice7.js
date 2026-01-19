f261940f562bdb1828f1100a4121255df9d1db56
Ashutosh Patil <123103890+AshutoshPatil14@users.noreply.github.com>
2026-01-19 20:21:25 +0530

    Update

--- practice/practice7.js
+++ practice/practice7.js
@@ -53,17 +53,17 @@
 
 const array = [1, 2, 3, 4, 5];
 
-// const reverseArray = (arr) => {
-//   let start = 0;
-//   let end = arr.length - 1;
-//   while (start < end) {
-//     let temp = arr[start];
-//     arr[start] = arr[end];
-//     arr[end] = temp;
-//     start++;
-//     end--;
-//   }
-//   return arr;
-// };
+const reverseArray = (arr) => {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start < end) {
+    let temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+  }
+  return arr;
+};
 
-// console.log(reverseArray(array));
+console.log(reverseArray(array));
