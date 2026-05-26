80638487b1c67e3b7594cc1c18c1de201828d140
Ashutosh Patil <123103890+AshutoshPatil14@users.noreply.github.com>
2026-05-26 20:44:34 +0530

    Update practice8.js

--- practice/practice8.js
+++ practice/practice8.js
@@ -30,23 +30,50 @@
 
 // Find the two numbers in an array that sum to a target.
 
-let arr = [1, 2, 3, 4, 5, 6, 7];
-const target = 9;
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// const target = 9;
 
-function twoSum(arr, target) {
-  const map = new Map();
+// function twoSum(arr, target) {
+//   const map = new Map();
 
-  for (let i = 0; i < arr.length; i++) {
-    const complement = target - arr[i];
+//   for (let i = 0; i < arr.length; i++) {
+//     const complement = target - arr[i];
 
-    if (map.has(complement)) {
-      return [complement, arr[i]];
+//     if (map.has(complement)) {
+//       return [complement, arr[i]];
+//     }
+
+//     map.set(arr[i], i);
+//   }
+
+//   return null;
+// }
+
+// console.log(twoSum([2, 7, 11, 15], 9));
+
+// const digits = [1, 2, 3];
+
+// const plusOne = (arr) => {
+//   let joinedArr = arr.join("");
+//   joinedArr++;
+//   return joinedArr.toString().split("").map(Number);
+// };
+
+// console.log(plusOne(digits));
+
+let arr = [1, [2, [3, 4]]];
+
+const flatArr = (arr) => {
+  let resArr = []
+
+  for (const i of arr) {
+    if(Array.isArray(i)){
+      resArr.push(...flatArr(i))
+    }else{
+      resArr.push(i)
     }
-
-    map.set(arr[i], i);
   }
-
-  return null;
+  return resArr
 }
 
-console.log(twoSum([2, 7, 11, 15], 9));
+console.log(flatArr(arr))
