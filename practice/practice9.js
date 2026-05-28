c50a50e214fc0e3083b51ccd36d4d02ca610142d
Ashutosh Patil <123103890+AshutoshPatil14@users.noreply.github.com>
2026-05-28 20:59:16 +0530

    Practice

---
+++ practice/practice9.js
@@ -0,0 +1,48 @@
+// Find the larget element in an array
+
+// let arr = [10, 342, 20, 4, 45, 99];
+
+// const findLargest = (arr) => {
+//   let largest = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (largest < arr[i]) largest = arr[i];
+//   }
+//   return largest;
+// };
+
+// console.log(findLargest(arr));
+
+// console.log(a);
+
+// const res = a * 5;
+// console.log(res)
+
+// var a = 10;
+
+// console.log(a)
+
+// const nums = [2, 7, 11, 15];
+
+// const target = 22;
+
+// for (let i = 0; i < nums.length; i++) {
+//   const find = target - nums[i];
+
+//   if (nums.includes(find)) {
+//     console.log([nums[i], find]);
+//     console.log(i, nums.indexOf(find))
+//     break;
+//   }
+// }
+
+// reverse a string
+
+// const str = "hello";
+
+// const revStr = [];
+
+// for (let i = str.length - 1; i >= 0; i--) {
+//   revStr.push(str[i]);
+// }
+// const newStr = revStr.join("");
+// newStr;
